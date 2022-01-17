556602db29fa8c928a9d25ec6f21f1a8b235f534
Rafael <rafaelferreira.0096@gmail.com>
2022-01-17 17:41:23 -0300

    Exercícios Spring

---
+++ SPRING - Aula 1/atividade2/src/test/java/com/atividade2/atividade2/Atividade2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade2.atividade2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
